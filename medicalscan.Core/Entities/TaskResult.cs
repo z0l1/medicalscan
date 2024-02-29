b65b085e8b8277ab8ba8dc430b9c85e35f98e443
z0l1 <andzo1998@gmail.com>
2024-02-29 20:51:56 +0100

    TaskResult for error handling

---
+++ medicalscan.Core/Entities/TaskResult.cs
@@ -0,0 +1,13 @@
+﻿namespace medicalscan.Core.Entities;
+
+public class TaskResult<T>
+{
+    public TaskResult(T? data = default, string? error = null, string? errorPrefix = null)
+    {
+        Data = data;
+        Error = $"{errorPrefix}: {error}";
+    }
+        
+    public T? Data { get; set; }
+    public string? Error { get; set; }
+}
