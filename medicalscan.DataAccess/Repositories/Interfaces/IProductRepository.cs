db2fc2172ddd9621b48b85bc2b766a1eb77af2f4
z0l1 <andzo1998@gmail.com>
2024-02-29 21:03:25 +0100

    repository abstraction layer "in case we ever need to replace the current store"

---
+++ medicalscan.DataAccess/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using medicalscan.Core.Entities;
+
+namespace medicalscan.Repository.Repositories.Interfaces;
+
+public interface IProductRepository
+{
+    public Task<TaskResult<Product?>> CreateProductAsync(string name, decimal price);
+    public Task<TaskResult<Product?>> GetProductByIdAsync(long id);
+    public Task<TaskResult<IEnumerable<Product>>> GetAllProductsAsync();
+    public Task<TaskResult<Product?>> UpdateProductAsync(Product entity);
+    public Task<TaskResult<bool>> DeleteProductByIdAsync(long id);
+}
--- medicalscan.DataAccess/Repositories/ProductRepository.cs
+++ medicalscan.DataAccess/Repositories/ProductRepository.cs
@@ -1,6 +1,38 @@
-﻿namespace medicalscan.Repository.Repositories;
+﻿using medicalscan.Core.Entities;
+using medicalscan.Repository.Repositories.Interfaces;
+using medicalscan.Repository.Store;
 
-public class ProductRepository
+namespace medicalscan.Repository.Repositories;
+
+public class ProductRepository: IProductRepository
 {
+    private AsyncMemoryProductStore GetStore()
+    {
+        return AsyncMemoryProductStore.Instance;
+    }
     
+    public Task<TaskResult<Product?>> CreateProductAsync(string name, decimal price)
+    {
+        return GetStore().CreateProduct(name, price);
+    }
+
+    public Task<TaskResult<Product?>> GetProductByIdAsync(long id)
+    {
+        return GetStore().GetProductById(id);
+    }
+
+    public Task<TaskResult<IEnumerable<Product>>> GetAllProductsAsync()
+    {
+        return GetStore().GetAllProducts();
+    }
+
+    public Task<TaskResult<Product?>> UpdateProductAsync(Product entity)
+    {
+        return GetStore().UpdateProduct(entity);
+    }
+
+    public Task<TaskResult<bool>> DeleteProductByIdAsync(long id)
+    {
+        return GetStore().DeleteProductById(id);
+    }
 }
