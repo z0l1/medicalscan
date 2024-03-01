a03320264d321998358edcef6dc9c432501a2f3b
z0l1 <andzo1998@gmail.com>
2024-03-01 20:49:15 +0100

    introduced service layer

---
+++ MedicalScanBackend.DomainLogic/Services/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using MedicalScan.Core;
+using MedicalScan.Core.Entities;
+
+namespace MedicalScanBackend.DomainLogic.Services.Interfaces;
+
+public interface IProductService
+{
+    public Task<HandledResponse<Product?>> CreateProductAsync(string name, decimal price);
+    public Task<HandledResponse<Product?>> GetProductByIdAsync(long id);
+    public Task<HandledResponse<IEnumerable<Product>>> GetAllProductsAsync();
+    public Task<HandledResponse<Product?>> UpdateProductAsync(Product entity);
+    public Task<HandledResponse<bool>> DeleteProductByIdAsync(long id);
+}
