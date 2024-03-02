2f8200494a618bcd89dfbc23ddf5aa9868c13a2e
z0l1 <andzo1998@gmail.com>
2024-03-02 01:56:12 +0100

    renamed and moved validation errors

---
+++ MedicalScanBackend.Core/Errors/ProductValidationErrors.cs
@@ -0,0 +1,7 @@
+﻿namespace MedicalScanBackend.DomainLogic.Errors;
+
+public static class ProductValidationErrors
+{
+    public static string PriceNotPositive => "Product price must be a positive number.";
+    public static string NameTooShort => "Product name must be at least 3 characters.";
+}
